9be7ef6a549fdcfa3a88368ec298363f59542260
Zoro-chi <jagbetuyi001@gmail.com>
2023-04-10 12:15:09 +0000

    work on activities

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,17 +30,17 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-#* CLOUDWATCH LOGS
+# * CLOUDWATCH LOGS
 import watchtower
 import logging
 from time import strftime
 
-#* ROLLBAR 
+# * ROLLBAR
 import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
 
-#* Configuring Logger to Use CloudWatch
+# * Configuring Logger to Use CloudWatch
 # LOGGER = logging.getLogger(__name__)
 # LOGGER.setLevel(logging.DEBUG)
 # console_handler = logging.StreamHandler()
@@ -49,7 +49,7 @@ from flask import got_request_exception
 # LOGGER.addHandler(cw_handler)
 # LOGGER.info("test log")
 
-#* Initialize tracing and an exporter that can send data to Honeycomb
+# * Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
@@ -68,9 +68,9 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 cognito_jwt_token = CognitoJwtToken(
-    user_pool_id = os.getenv("AWS_COGNITO_USER_POOL_ID"),
-    user_pool_client_id = os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
-    region = os.getenv("AWS_DEFAULT_REGION"),
+    user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"),
+    user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+    region=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 # * X-RAY
@@ -84,11 +84,11 @@ frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
 cors = CORS(
-  app, 
-  resources={r"/api/*": {"origins": "*"}},
-  headers=['Content-Type', 'Authorization'], 
-  expose_headers='Authorization',
-  methods="OPTIONS,GET,HEAD,POST"
+    app,
+    resources={r"/api/*": {"origins": "*"}},
+    headers=['Content-Type', 'Authorization'],
+    expose_headers='Authorization',
+    methods="OPTIONS,GET,HEAD,POST"
 )
 
 # @app.after_request
@@ -97,8 +97,10 @@ cors = CORS(
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
 
-#* ROLLBAR CONFIG / INIT
+# * ROLLBAR CONFIG / INIT
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+
+
 @app.before_first_request
 def init_rollbar():
     """init rollbar module"""
@@ -115,10 +117,12 @@ def init_rollbar():
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
+
 @app.route('/rollbar/test')
 def rollbar_test():
     rollbar.report_message('Hello World!', 'warning')
     return "Hello World!"
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -167,13 +171,13 @@ def data_home():
     access_token = extract_access_token(request.headers)
     try:
         claims = cognito_jwt_token.verify(access_token)
-        #* authentication request
+        # * authentication request
         app.logger.debug("authenticated")
         app.logger.debug(claims)
         app.logger.debug(claims["username"])
         data = HomeActivities.run(cognito_user_id=claims["username"])
     except TokenVerifyError as e:
-        #* un-authenticated request
+        # * un-authenticated request
         app.logger.debug(e)
         app.logger.debug("un-authenticated")
         data = HomeActivities.run()
@@ -203,13 +207,12 @@ def data_search():
         return model['errors'], 422
     else:
         return model['data'], 200
-    return
 
 
 @app.route("/api/activities", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_activities():
-    user_handle = request.json["user_handle"]
+    user_handle = request.json['user_handle']
     message = request.json['message']
     ttl = request.json['ttl']
     model = CreateActivity.run(message, user_handle, ttl)
@@ -222,14 +225,14 @@ def data_activities():
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 def data_show_activity(activity_uuid):
-    data = ShowActivity.run(activity_uuid=activity_uuid)
+    data = ShowActivities.run(activity_uuid=activity_uuid)
     return data, 200
 
 
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST', 'OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-    user_handle = 'andrewbrown'
+    user_handle = request.json['user_handle']
     message = request.json['message']
     model = CreateReply.run(message, user_handle, activity_uuid)
     if model['errors'] is not None:
