f38947fe218017b42bd974e22849571760e3e2ef
Zoro-chi <jagbetuyi001@gmail.com>
2023-04-20 01:09:15 +0000

    fixed cognito uuid

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'andrew@test.com', 'MOCK'),
-  ('Zoro', 'zoro-chi', 'zoro@test.com', 'MOCK');
+  ('Zoro', 'zorochi', 'zoro@test.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
