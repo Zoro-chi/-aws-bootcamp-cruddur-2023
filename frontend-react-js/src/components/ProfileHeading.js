0520c9f05686a0d9c4ba36e3bebecb84712118b8
Zoro-chi <jagbetuyi001@gmail.com>
2023-06-04 01:16:05 +0000

    work on presigned url

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,6 +1,8 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
+import ProfileAvatar from 'components/ProfileAvatar'
+
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.zoro-chi.com.ng/banners/banner.jpg")';
   const styles = {
@@ -10,22 +12,19 @@ export default function ProfileHeading(props) {
   };
   return (
   <div className='activity_feed_heading profile_heading'>
-    <div className='title'>{props.profile?.display_name}</div>
-    <div className="cruds_count">{props.profile?.cruds_count} Cruds</div>
+    <div className='title'>{props.profile.display_name}</div>
+    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.zoro-chi.com.ng/avatars/data.jpg"></img>
-      </div>
+      <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
-        <div className="display_name">{props.profile?.display_name}</div>
-        <div className="handle">@{props.profile?.handle}</div>
+        <div className="display_name">{props.profile.display_name}</div>
+        <div className="handle">@{props.profile.handle}</div>
       </div>
       <EditProfileButton setPopped={props.setPopped} />
     </div>
-    <div className='bio'> {props.profile?.bio} </div>
-
+    <div className="bio">{props.profile.bio}</div>
 
   </div>
   );
