481382833cc0a64327bd60bea49dad36a121dc08
Zoro-chi <jagbetuyi001@gmail.com>
2023-07-15 13:22:59 +0000

    refactor: week-x clean up

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -4,7 +4,7 @@ VALUES
   ('Andrew Brown', 'andrewbrown', 'jagbetuyi+andrewbrown@hotmail.com', 'MOCK'),
   ('Junzy', 'JR', 'jagbetuyi001@gmail.com', 'MOCK'),
   ('Zoro', 'zorochi', 'jagbetuyi@hotmail.com', 'MOCK'),
-  ('Londo Mollari', 'londo', 'jagbetuyi+londo@hotmail.com', 'MOCK');
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
@@ -12,4 +12,9 @@ VALUES
     (SELECT uuid from public.users WHERE users.handle = 'zorochi' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'Luke I am your Fajjjeeerr!',
+    current_timestamp + interval '10 day'
+  );
